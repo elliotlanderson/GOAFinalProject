775adc961ac5e3e13f60d7eb1ae3b949bd47a99f
Elliot Anderson <anderson.elliot3@gmail.com>
2016-05-04 12:51:18 -0400

    Incorporated the MoveValidator
    I incorporated the MoveValidator into the GameController to actually check to make sure the moves are valid and prevent invalid moves from being executed.  I also added a game end gameState, to signify the end of the game.  As of know, the end of the game is signified by the capturing of one of hte kings, but once I add some AI logic or possible move logic I will be able to determine whether a checkmate state has been reached

--- src/ea/chessfinal/controller/GameController.java
+++ src/ea/chessfinal/controller/GameController.java
@@ -3,6 +3,10 @@ package ea.chessfinal.controller;
 /**
  * Created by elliotanderson on 5/3/16.
  * The ea.chessfinal.controller for the chess game
+ * This class is the GAME/BUSINESS logic for the chess game
+ * This should be completely separated from the view, or at least as
+ * abstracted as possible and within reason to the scope of this project.
+ * @todo clean up the code (as per usual)
  * @author Elliot Anderson
  */
 
@@ -20,17 +24,27 @@ public class GameController {
      */
     public static final int GAME_STATE_WHITE = 0;
     public static final int GAME_STATE_BLACK = 1;
+    public static final int GAME_STATE_END = 2;
 
     private int gameState = GAME_STATE_WHITE; // default white starts
 
     // index 0 = bottom, size-1 = top
     private List<Piece> pieces = new ArrayList<Piece>();
 
+    /**
+     * @var instance of the move validator
+     */
+    private MoveValidator moveValidator;
+
 
     /**
-     * initialize the game
+     * initialize the game and set class instance variables
      */
     public GameController() {
+
+        // instantiate the move validator
+        this.moveValidator = new MoveValidator();
+
         // create and place white pieces on board
 
         createAndAddPiece(Piece.WHITE_COLOR, Piece.PIECE_ROOK, Board.ROW_1, Board.COLUMN_A);
@@ -65,6 +79,7 @@ public class GameController {
             createAndAddPiece(Piece.BLACK_COLOR, Piece.PIECE_PAWN, Board.ROW_7, currentColumn);
             currentColumn++;
         }
+
     }
 
     /**
@@ -81,13 +96,23 @@ public class GameController {
 
     /**
      * Move the piece to the specified location.  If the target already has a piece in it
-     * then that piece is marked as "captured"
+     * then that piece is marked as "captured".  If the move cannot be successfully done because
+     * of constraints or piece rules, return false
+     *
      * @param fromRow the row the piece is being moved from
      * @param fromCol the col the piece is being moved from
      * @param toRow the row the piece is being moved to
      * @param toCol the column the piece is being moved to
+     * @return true if the piece moved successfully, false if it wasn't
      */
-    public void movePiece(int fromRow, int fromCol, int toRow, int toCol) {
+    public boolean movePiece(int fromRow, int fromCol, int toRow, int toCol) {
+
+        if (! this.moveValidator.isMoveValid(fromRow, fromCol, toRow, toCol)) {
+            // move is invalid
+            System.out.println("move invalid");
+            return false;
+        }
+
         Piece piece = getNonCapturedPieceAtLocation(fromRow, fromCol);
 
         // check to see if the move is capturing an opponent's piece
@@ -100,6 +125,30 @@ public class GameController {
 
         piece.setRow(toRow);
         piece.setColumn(toCol);
+
+        if (isGameEndConditionReached()) {
+            this.gameState = GAME_STATE_END;
+        } else {
+            this.changeGameState();
+        }
+
+        return true;
+    }
+
+    /**
+     * Check if the condition for the game ending has been met: one king has been captured
+     * YES, i understand this is an oversimplification
+     * @todo Add functionality for a "checkmate" so the user doesn't actually have to capture the king
+     * @return true if the conditions show the game has ended (king captured)
+     */
+    private boolean isGameEndConditionReached() {
+        for (Piece piece : this.pieces) {
+            if (piece.getType() == Piece.PIECE_KING && piece.isCaptured()) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     /**
@@ -160,12 +209,28 @@ public class GameController {
      * @note the reason I use a switch here (as opposted to if-else) is for scalability
      */
     public void changeGameState() {
+
+        // check to see if the game has ended (based on the game end condition)
+        if (this.isGameEndConditionReached()) {
+            if (this.gameState == GameController.GAME_STATE_BLACK) {
+                System.out.println("Game Over!  Black Won!");
+            } else {
+                System.out.println("Game Over! White Won!");
+            }
+
+            this.gameState = GameController.GAME_STATE_END;
+        }
+
         switch (this.gameState) {
             case GAME_STATE_BLACK:
                 this.gameState = GAME_STATE_WHITE;
                 break;
             case GAME_STATE_WHITE:
                 this.gameState = GAME_STATE_BLACK;
+                break;
+            case GAME_STATE_END:
+                //don't change anymore
+                break;
             default:
                 throw new IllegalStateException("Unknown game state");
         }
